ed5c16e32db3c930755d6103968fa89ff423cfb1
Joel Robert Justiawan <joelwindows7@gmail.com>
2021-06-28 00:55:00 +0700

    [skip ci] start the chartening
    getting freaky on the friday night yeah
    
    somebody had the test json file yoinked, perhaps we can take that.
    
    oh, did we said start from scratch for the Test.ogg? um change of plan. I think. I'm already tired that I've been going to build 3 UI musics (getting freaky, Game Over, Breakfast). now the last one, Test, I think that should be safe. idk.

---
+++ assets/preload/data/stages/empty-template/stageScript.lua
@@ -0,0 +1,31 @@
+-- inspire from Kade Engine modchart lua
+-- JOELwindows7's new stage chart script
+
+-- when the state has just loaded, do this background
+function loaded(song)
+
+end
+
+-- when the countdown finished and song start
+function start(song)
+    print("Song: " .. song .. " @ " .. bpm .. " downscroll: " .. downscroll)
+    print("\nStage: " .. stage)
+end
+
+function update(elapsed)
+
+end
+
+function beatHit(beat)
+
+end
+
+function stepHit(beat)
+
+end
+
+function keyPressed(key)
+    
+end
+
+print("Stage Chart script loaded! :) cool and good.")
